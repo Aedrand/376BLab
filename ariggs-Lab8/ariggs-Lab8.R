6571c6d1882502be41c4f78604fc89ac8ecee2f0
Riggs <ariggs@csr06.cs.skidmore.edu>
2017-04-05 17:06:05 -0400

    Tried some new things with C4.5, still seems broken

--- ariggs-Lab8/ariggs-Lab8.R
+++ ariggs-Lab8/ariggs-Lab8.R
@@ -43,33 +43,27 @@ writeLines("Summary of testing set:")
 summary(train)
 
 ###Data Preparation###
-#Create sets of data where the target is nominal to allow C4.5 creation
-#Remove id column and a2-7
-train.nominal <- train[,2:13]
-train.nominal$a1 <- as.factor(train.nominal$a1)
-test.nominal <- test[,2:13]
-test.nominal$a1 <- as.factor(test.nominal$a1)
-
-#Remove id column and a1,a3-7
-train.nominal2 <- train[,2:14]
-train.nominal2$a1 <- NULL
-train.nominal2$a2 <- as.factor(train.nominal2$a2)
-test.nominal2 <- test[,2:14]
-test.nominal2$a1 <- NULL
-test.nominal2$a2 <- as.factor(test.nominal2$a2)
-
-#Create a combined dataset for binning
+#Create a combined dataset
 whole <- rbind(train,test)
 whole2 <- rbind(train,test)
+whole.c <- whole
+whole.c2 <- whole2
 
-#Use equal frequency binning to bin a1 in whole and a2 in whole2
-whole$a1 <- discretize(whole$a1, categories = 10)
-whole2$a2 <- discretize(whole$a2, categories = 10)
+#Use equal width binning to bin a1 in whole and a2 in whole2
+whole$a1 <- discretize(whole$a1,categories = 10)
+whole2$a2 <- discretize(whole$a2,categories = 10)
+
+#Convert the combined datasets to nominal, so they can be used in C4.5
+whole.c$a1 <- as.factor(whole.c$a1)
+whole.c2$a2 <- as.factor(whole.c2$a2)
 
 #Remove the target features that are not being used in each set
 whole <- whole[,2:13]
 whole2 <- whole2[2:14]
 whole2$a1 <- NULL
+whole.c <- whole.c[,2:13]
+whole.c2 <- whole.c2[2:14]
+whole.c2$a1 <- NULL
 
 #Split the whole dataset into training and testing again
 set.seed(1)
@@ -79,13 +73,21 @@ split.test <- whole[-split,]
 split2 <- createDataPartition(whole2$a2, p=0.6)[[1]]
 split.train2 <- whole2[split2,]
 split.test2 <- whole2[-split2,]
+split.c <- createDataPartition(whole.c$a1, p=0.6)[[1]]
+split.c.train <- whole.c[split.c,]
+split.c.test <- whole.c[-split.c,]
+split.c2 <- createDataPartition(whole.c2$a2, p=0.6)[[1]]
+split.c2.train <- whole.c2[split.c2,]
+split.c2.test <- whole.c2[-split.c2,]
 rm(split)
 rm(split2)
+rm(split.c)
+rm(split.c2)
 
 ###Model Creation###
 ##C4.5##
 # Build a decision tree for a1 using C4.5 (Weka's J48 implementation)
-train.model.nom <- J48(a1 ~ ., data=train.nominal)
+train.model.nom <- J48(a1 ~ ., data=split.c.train)
 
 # View details of the constructed tree
 #summary(train.model.nom)
@@ -94,7 +96,7 @@ train.model.nom <- J48(a1 ~ ., data=train.nominal)
 #plot(train.model.nom)
 
 # Build a decision tree for a2 using C4.5 (Weka's J48 implementation)
-train.model.nom2 <- J48(a2 ~ ., data=train.nominal2)
+train.model.nom2 <- J48(a2 ~ ., data=split.c2.train)
 
 # View details of the constructed tree
 #summary(train.model.nom2)
@@ -116,10 +118,12 @@ print(train.model.rules2)
 ###Evaluation###
 ##C4.5##--------------------------------------------------------------------------NOT WORKING
 #Create predicitions for a1 and a2 using the test set
-test.predict.nom <- predict(train.model.nom, test.nominal)
+test.predict.nom <- predict(train.model.nom, split.c.test)
 
 #Calculate the performance of a1 and a2 C4.5 models
-test.eval.nom <- confusionMatrix(test.predict.nom, test.nominal$a1)
+test.eval.nom <- confusionMatrix(test.predict.nom, split.c.test$a1)
+
+print(test.eval.nom)
 
 ##RIPPER##
 #Create predictions for a1 and a2 using the new test set
@@ -130,4 +134,8 @@ test.predict.rules2 <- predict(train.model.rules2, split.test2)
 test.eval.rules <- confusionMatrix(test.predict.rules, split.test$a1)
 test.eval.rules2 <- confusionMatrix(test.predict.rules2, split.test2$a2)
 
+#Print the result of the evaluations
+writeLines("RIPPER a1:")
 print(test.eval.rules)
+writeLines("RIPPER a2:")
+print(test.eval.rules2)
